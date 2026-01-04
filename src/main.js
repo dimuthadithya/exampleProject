e60e7f2ac84051a6c41c1307e8f5fc0c59b49887
dimuthadithya <dimuthx353@gmail.com>
2026-01-04 09:50:36 +0530

    feat: initialize project with Vite, Tailwind CSS, and DaisyUI
    - Added package.json with project metadata and dependencies
    - Created main.js to set up the application structure and UI components
    - Added style.css to include Tailwind CSS and DaisyUI styles
    - Configured Vite with Tailwind CSS plugin in vite.config.js

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+<div class="navbar bg-base-100 shadow-sm">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1">
+      <li><a>Link</a></li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul class="bg-base-100 rounded-t-none p-2">
+            <li><a>Link 1</a></li>
+            <li><a>Link 2</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </div>
+</div>
+ <div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content flex-col lg:flex-row">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      class="max-w-sm rounded-lg shadow-2xl"
+    />
+    <div>
+      <h1 class="text-5xl font-bold">Box Office News!</h1>
+      <p class="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+`;
